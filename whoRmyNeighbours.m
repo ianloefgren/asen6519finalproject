2021409ad5b4ebd5daf583ba022cfd1a7b2adc0a
oferdagan <42900170+oferdagan@users.noreply.github.com>
2019-04-04 12:09:19 -0600

    Update whoRmyNeighbours.m
    Added the current cell as a "neighbour"

--- whoRmyNeighbours.m
+++ whoRmyNeighbours.m
@@ -6,7 +6,8 @@ function [c] = whoRmyNeighbours(ni,nj)
 % nj - # of grid cells in the n direction
 
 
-% Neighbours could be the next cell horizontally, vertically and % diagonally
+% Neighbours could be the next cell horizontally, vertically, diagonally
+% and the same cell
 
 % ij is the current cel number
 
@@ -14,23 +15,23 @@ for ii=1:ni
     for jj=1:nj
         ij = (jj-1)*ni+ii;
         if ii==1 && jj==1
-            c{ij,1} = [2, ni+1, ni+2];
+            c{ij,1} = [ij, 2, ni+1, ni+2];
         elseif ii==ni && jj==1
-            c{ij,1} = [ni-1, 2*ni-1, 2*ni];
+            c{ij,1} = [ij,ni-1, 2*ni-1, 2*ni];
         elseif jj==1
-            c{ij,1} = [ii-1, ii+1, ii+ni-1, ii+ni, ii+ni+1];
+            c{ij,1} = [ij,ii-1, ii+1, ii+ni-1, ii+ni, ii+ni+1];
         elseif ii==1 && jj~=nj
-            c{ij,1} = [ij-ni, ij-ni+1,ij+1, ij+ni, ij+ni+1];
+            c{ij,1} = [ij,ij-ni, ij-ni+1,ij+1, ij+ni, ij+ni+1];
         elseif ii==1  % jj=nj
-            c{ij,1} = [ij-ni, ij-ni+1, ij+1];
+            c{ij,1} = [ij,ij-ni, ij-ni+1, ij+1];
         elseif ii==ni && jj~=nj
-            c{ij,1} = [ij-ni-1, ij-ni, ij-1, ij+ni-1, ij+ni];
+            c{ij,1} = [ij,ij-ni-1, ij-ni, ij-1, ij+ni-1, ij+ni];
         elseif ii==ni % jj=nj
-            c{ij,1} = [ij-ni-1,ij-ni, ij-1];
+            c{ij,1} = [ij,ij-ni-1,ij-ni, ij-1];
         elseif jj==nj
-            c{ij,1} = [ij-ni-1, ij-ni, ij-ni+1, ij-1,ij+1];
+            c{ij,1} = [ij,ij-ni-1, ij-ni, ij-ni+1, ij-1,ij+1];
         else
-            c{ij,1} = [ij-ni-1, ij-ni, ij-ni+1, ij-1,ij+1 , ij+ni-1, ij+ni, ij+ni+1];
+            c{ij,1} = [ij,ij-ni-1, ij-ni, ij-ni+1, ij-1,ij+1 , ij+ni-1, ij+ni, ij+ni+1];
         
         end
     end
