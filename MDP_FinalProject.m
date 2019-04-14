142dbe6cd5760fdbef87929980c3666878f26e99
oferdagan <42900170+oferdagan@users.noreply.github.com>
2019-04-13 22:44:37 -0600

    Update MDP_FinalProject.m
    Added transition matrix
    Added utility, for now utility "explodes"

--- MDP_FinalProject.m
+++ MDP_FinalProject.m
@@ -30,6 +30,49 @@ end
 X=eval(['combvec(',str,')']);
 
 
+% Build Action-space:
+v = 1:5;
+str = 'v';
+for ii=1:nAgents-1
+    str = [str,',v'];
+end
+A=eval(['combvec(',str,')']);
+
+
+for s=1:size(X,2)  % States
+    for a=1:size(A,2)  % Actions
+        if s==1
+            T{a} = zeros(size(X,2),size(X,2));
+        end
+        stateSize=1;
+        str = '';
+        for ii= 1:nAgents
+            Ptmp(ii,:) = P{A(ii,a)}(X(ii+nTargets,s),:);
+            indCell{ii} = find(Ptmp(ii,:)~=0);
+            stateSize = stateSize*size(indCell{ii},2);
+            str = [str,['indCell{',num2str(ii),'},']];
+        end
+        
+        TmpStateVec = eval(['combvec(',str(1:end-1),')']);
+        
+        
+            for jj=1:size(TmpStateVec,2)
+                if nAgents>1
+                    [row,stateInd] = find(sum(X(nTargets+1:end,:)==TmpStateVec(:,jj))==nAgents);
+                else
+                    [row,stateInd] = find(X(nTargets+1:end,:)==TmpStateVec(:,jj));
+                end
+                if sum(T{a}(s,stateInd))==0
+                    T{a}(s,stateInd)=1;
+                end
+                for nn=1:nAgents                
+                    T{a}(s,stateInd) = T{a}(s,stateInd)*Ptmp(1,TmpStateVec(nn,jj));
+                end
+            end
+        
+    end
+end
+
 
 % Build Reward 
 R = 0*ones(size(X,2),1);
@@ -59,17 +102,26 @@ for jj=1:size(X,2)
     interCount = zeros(nTargets,1);
 end
 
-
+% S=sparse(T{a});
+%  [i,j,k]=find(S);
 
 % Utility
-U = min(R(:,1))*ones(N,1);
+U = min(R(:,1))*ones(size(R));
 
 for k=1:1000
-    for ii=1:N
-        U(ii,k+1) = R(ii,k)+max 
-    
+    Utmp = zeros(1,size(A,2));
+    for s=1:size(X,2)  % current state
+        for a=1:size(A,2)   % actions
+            for sp1 = 1:size(X,2) % next state
+                Utmp(1,a) = Utmp(1,a)+T{a}(s,sp1)*U(sp1,k);
+            end
+        end
+        U(s,k+1) = R(s,1)+gamma*max(Utmp(1,:),[],2);
+    end
+   
     
 end
+
 
 
 
