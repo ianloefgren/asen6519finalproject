26715ebc2d80a2ca2c967f370a7f12ee7705bd78
oferdagan <42900170+oferdagan@users.noreply.github.com>
2019-04-15 11:49:11 -0600

    Update BuildTransitionMatrix.m
    Changed action order to make "stay" first

--- BuildTransitionMatrix.m
+++ BuildTransitionMatrix.m
@@ -11,7 +11,10 @@ T = zeros(N,N);
 for ii=1:ni
    for jj=1:nj
        s = (jj-1)*ni+ii;
-       if a==1 % up
+       if a==1 % stay
+           T(s,s)=0.8;
+           T(s,c{s}(c{s}~=s)) = (1-T(s,s))/(length(c{s})-1);
+       elseif a==2 % up
            if ii==ni %top row
             T(s,s)=0.8;
             T(s,c{s}(c{s}~=s)) = (1-T(s,s))/(length(c{s})-1);         
@@ -19,7 +22,7 @@ for ii=1:ni
                T(s,s+1)=0.8;
                T(s,c{s}(c{s}~=(s+1))) = (1-T(s,s+1))/(length(c{s})-1);
            end
-       elseif a==2 % down
+       elseif a==3 % down
            if ii==1 %bottom row
             T(s,s)=0.8;
             T(s,c{s}(c{s}~=s)) = (1-T(s,s))/(length(c{s})-1);         
@@ -27,7 +30,7 @@ for ii=1:ni
                T(s,s-1)=0.8;
                T(s,c{s}(c{s}~=(s-1))) = (1-T(s,s-1))/(length(c{s})-1);
            end       
-       elseif a==3 % right
+       elseif a==4 % right
            if jj==nj %right column
             T(s,s)=0.8;
             T(s,c{s}(c{s}~=s)) = (1-T(s,s))/(length(c{s})-1);         
@@ -35,7 +38,7 @@ for ii=1:ni
                T(s,s+ni)=0.8;
                T(s,c{s}(c{s}~=(s+ni))) = (1-T(s,s+ni))/(length(c{s})-1);
            end          
-       elseif a==4 % left
+       elseif a==5 % left
             if jj==1 %left column
             T(s,s)=0.8;
             T(s,c{s}(c{s}~=s)) = (1-T(s,s))/(length(c{s})-1);         
@@ -43,9 +46,7 @@ for ii=1:ni
                T(s,s-ni)=0.8;
                T(s,c{s}(c{s}~=(s-ni))) = (1-T(s,s-ni))/(length(c{s})-1);
            end                     
-       elseif a==5 % stay
-           T(s,s)=0.8;
-           T(s,c{s}(c{s}~=s)) = (1-T(s,s))/(length(c{s})-1);      
+             
        end
    end
     
