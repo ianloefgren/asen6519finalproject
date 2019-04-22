b08c9474af4903d3df2e46ad4aca73fa0f3a7fee
oferdagan <42900170+oferdagan@users.noreply.github.com>
2019-04-22 12:08:04 -0600

    Update BuildTransitionMatrix2.m
    Fixed a bug in the stationary target section

--- BuildTransitionMatrix2.m
+++ BuildTransitionMatrix2.m
@@ -29,11 +29,11 @@ TmpStateVec = eval(['combvec(',str(1:end-1),')']);
 for jj=1:size(TmpStateVec,2)             
     
     if DynamicModel==2  % still target  - % Only states where target states are stationary
-        if nAgents+nTargets>2
+%         if nAgents+nTargets>2
             [row,stateInd] = find(sum(X==[X(1:nTargets,s) ; TmpStateVec(:,jj)])==(nTargets+nAgents));
-        else
-            [row,stateInd] = find(X==[X(1:nTargets,s) ; TmpStateVec(:,jj)]);
-        end
+%         else
+%             [row,stateInd] = find(X==[X(1:nTargets,s) ; TmpStateVec(:,jj)]);
+%         end
     else
      % All possible state wehre the targets are in different cells but the
     % agents states are constant
